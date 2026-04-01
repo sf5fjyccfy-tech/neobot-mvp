ca14bf349236a13a785363ce44f1c0555d4fcabb
tim <neobot561@gmail.com>
2026-04-01 03:09:15 +0100

    fix: pin python 3.11, fix webpack @/ alias for Render build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const { withSentryConfig } = require('@sentry/nextjs');
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    // Garantit la résolution de l'alias @/ même quand withSentryConfig wraps la config
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = withSentryConfig(nextConfig, {
